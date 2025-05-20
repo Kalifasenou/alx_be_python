7549fa0e24dcc2ae3cb224764809845eae141f4f
KSENOU <kalifasenou10@gmail.com>
2025-05-20 11:28:53 +0000

    add of different operation in the basic_operations file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Déf of variables
+number1 = 10
+number2 = 5
+
+# Operation
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print of all operation 
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")

5b692d4cb5f58d23f172e482306de0eaf6a2edfe
KSENOU <kalifasenou10@gmail.com>
2025-06-28 14:31:10 +0000

    creation du fichier sql pour Insert a single row into the Customers table.

---
+++ Intro_to_DB/task_5.sql
@@ -0,0 +1,4 @@
+-- Insert a single row into the Customers table.
+-- The database name 'alx_book_store' will be passed as a MySQL command argument.
+INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
